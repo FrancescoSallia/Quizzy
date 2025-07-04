c07022bc98dc533f9e84c63cc58b4a07758a4b4e
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-07-04 18:13:03 +0200

    progressView WIP

--- app/src/main/java/com/example/quizzy/MainActivity.kt
+++ app/src/main/java/com/example/quizzy/MainActivity.kt
@@ -4,21 +4,50 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-//        val splashScreen = installSplashScreen()
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navController: NavController
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
-          // Zugriff auf den NavHostFragment
+        // Zugriff auf den NavHostFragment
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
 
+
+
+//region NavigationDrawer
+        navController = navHostFragment.navController
+
+        // DrawerLayout und NavigationView referenzieren (IDs aus deinem Layout anpassen)
+        drawerLayout = findViewById(R.id.drawerLayout)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+
+        // Menü Item Klicks abfangen und navigieren
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Navigiere zu Fragment mit Menü-ID (entspricht fragment ID im NavGraph)
+            navController.navigate(menuItem.itemId)
+
+            // Drawer schließen
+            drawerLayout.closeDrawer(GravityCompat.END)
+            true
+        }
+
+//endregion
+
+
     }
-
-
 }
---
+++ app/src/main/java/com/example/quizzy/ui/ProgressFragment.kt
@@ -0,0 +1,31 @@
+package com.example.quizzy.ui
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.quizzy.R
+import com.example.quizzy.databinding.FragmentCompleteBinding
+import com.example.quizzy.databinding.FragmentProgressBinding
+
+class ProgressFragment : Fragment() {
+
+    private lateinit var vb: FragmentProgressBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        vb = FragmentProgressBinding.inflate(inflater, container, false)
+        return vb.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
