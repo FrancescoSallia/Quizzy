098dfce017d3a0caa28bc76aa148ee435911d625
FrancescoSallia <128042494+FrancescoSallia@users.noreply.github.com>
2025-07-05 13:10:26 +0200

    statusbar color changed on spashScreen

--- app/src/main/java/com/example/quizzy/ui/SplashScreen/SplashActivity.kt
+++ app/src/main/java/com/example/quizzy/ui/SplashScreen/SplashActivity.kt
@@ -2,17 +2,18 @@ package com.example.quizzy.ui.SplashScreen
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.os.postDelayed
 import com.example.quizzy.MainActivity
 import com.example.quizzy.R
-import android.os.Handler
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        window.statusBarColor = getColor(R.color.background)
 
         // Optional: Splash verzögern (z.B. 2 Sekunden)
         Handler(Looper.getMainLooper()).postDelayed({
